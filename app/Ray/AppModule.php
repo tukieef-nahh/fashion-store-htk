ad7474e5f8351e366c16a24a5c211ac5ff0ae50b
[t]kieu <thanhkieu.tttk1982003@gmail.com>
2024-11-21 18:37:30 +0700

    Add files via upload

---
+++ app/Ray/AppModule.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Ray;
+
+use Ray\Di\AbstractModule;
+use App\Services\SanPhamService;
+use App\Aspects\LoggingInterceptor;
+
+class AppModule extends AbstractModule
+{
+    protected function configure()
+    {
+        // Bind SanPhamService
+        $this->bind(SanPhamService::class);
+
+        // Apply LoggingInterceptor to SanPhamService methods
+        $this->bindInterceptor(
+            $this->matcher->subclassesOf(SanPhamService::class), // Áp dụng cho các lớp con của SanPhamService
+            $this->matcher->any(), // Chặn mọi phương thức
+            [LoggingInterceptor::class] // Sử dụng LoggingInterceptor
+        );
+    }
+}
---
+++ app/Services/SanPhamService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\SanPham;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class SanPhamService
+{
+    public function index($request)
+    {
+        $query = SanPham::with(['DanhMuc', 'ThuongHieu']);
+
+        if ($request->has('SearchString') && !empty($request->SearchString)) {
+            $query->where('tenSP', 'like', '%' . $request->SearchString . '%');
+        }
+
+        $sortField = $request->get('sortField', 'id');
+        $sortOrder = $request->get('sortOrder', 'asc');
+        if (in_array($sortField, ['tenSP', 'gia', 'danh_muc_id', 'thuong_hieu_id'])) {
+            $query->orderBy($sortField, $sortOrder);
+        }
+
+        return $query->paginate(5);
+    }
+
+    public function store($data)
+    {
+        $slug = Str::slug($data['tenSP']);
+        $imageName = $data['image'] ?? null;
+
+        if (!empty($data['image'])) {
+            $imageName = Str::slug($data['tenSP']) . '-' . time() . '.' . $data['image']->extension();
+            $data['image']->move(public_path('source/image/sanpham'), $imageName);
+        }
+
+        return SanPham::create([
+            'tenSP' => $data['tenSP'],
+            'slug' => $slug,
+            'moTa' => $data['moTa'],
+            'gia' => $data['gia'],
+            'soLuong' => $data['soLuong'],
+            'danh_muc_id' => $data['danh_muc_id'],
+            'thuong_hieu_id' => $data['thuong_hieu_id'],
+            'image' => $imageName,
+        ]);
+    }
+
+    public function update($sanpham, $data)
+    {
+        $newImageName = null;
+
+        if (!empty($data['image'])) {
+            $oldImagePath = public_path('source/image/sanpham/' . $sanpham->image);
+            if (file_exists($oldImagePath)) {
+                unlink($oldImagePath);
+            }
+
+            $newImageName = Str::slug($data['tenSP']) . '-' . time() . '.' . $data['image']->extension();
+            $data['image']->move(public_path('source/image/sanpham'), $newImageName);
+        }
+
+        $sanpham->update([
+            'tenSP' => $data['tenSP'],
+            'slug' => Str::slug($data['tenSP']),
+            'moTa' => $data['moTa'],
+            'gia' => $data['gia'],
+            'soLuong' => $data['soLuong'],
+            'danh_muc_id' => $data['danh_muc_id'],
+            'thuong_hieu_id' => $data['thuong_hieu_id'],
+            'image' => $newImageName ?? $sanpham->image,
+        ]);
+
+        return $sanpham;
+    }
+
+    public function destroy($sanpham)
+    {
+        $imagePath = public_path('source/image/sanpham/' . $sanpham->image);
+
+        if ($sanpham->image && file_exists($imagePath)) {
+            unlink($imagePath);
+        }
+
+        $sanpham->delete();
+        return true;
+    }
+}
